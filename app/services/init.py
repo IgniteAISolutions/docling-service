ec8fae2c1cc12b2bda2c8a813207ece5f6ed0686
IgniteAI <Christopher.Duffy339@gmail.com>
2025-11-16 17:27:27 +0000

    Initialize document processing services module

---
+++ app/services/init.py
@@ -0,0 +1,6 @@
+"""
+Document processing services
+"""
+from .document_processor import document_processor, DocumentProcessingService
+
+__all__ = ["document_processor", "DocumentProcessingService"]
