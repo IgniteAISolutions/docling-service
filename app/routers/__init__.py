c18a118bbb4d0f9ebb63fd5d2e9180f012989a44
root <root@docling-git-u53318.vm.elestio.app>
2025-11-17 02:23:11 +0000

    Remove PDF from routers __init__ AGAIN

--- app/routers/__init__.py
+++ app/routers/__init__.py
@@ -1,7 +1,6 @@
 """
 API Routers for Universal Product Automation
 """
-from .pdf import router as pdf_router
 from .image import router as image_router
 from .sku import router as sku_router
 from .url import router as url_router
@@ -10,7 +9,6 @@ from .export_csv import router as export_csv_router
 from .seo import router as seo_router
 
 __all__ = [
-    "pdf_router",
     "image_router",
     "sku_router",
     "url_router",
